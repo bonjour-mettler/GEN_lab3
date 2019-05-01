33415e9da9672b7a794a9020841d36a84ae0e350
mbonjour <mic.bonjour@hotmail.fr>
2019-05-01 10:05:35 +0200

    adding tests for Piece class

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,4 +8,25 @@ class PieceTest {
         Piece test = new Piece("test", null);
         assertEquals("test", test.getName());
     }
+
+    @Test
+    void aPieceShouldRetrieveAGoodLocationAndName(){
+        Square sq = new Square("Square Test");
+        Piece test = new Piece("test", sq);
+        assertEquals("test", test.getName());
+        assertEquals("Square Test", test.getLocation().getName());
+    }
+
+    @Test
+    void aPieceShouldChangeLocationCorrectly(){
+        Board boardTest = new Board();
+        Piece test = new Piece("test", boardTest.getGo());
+        for(int i = 0; i < 40; ++i){
+            Square newLoc = boardTest.getSquare(boardTest.getGo(), i);
+            test.setLocation(newLoc);
+            assertEquals(newLoc, test.getLocation());
+        }
+        assertEquals("test", test.getName());
+    }
+
 }
